75d9623d70128084d6e23fc38975507da884e3a7
J SHAKTHI SRAVANTH <shakthisravanth001@gmail.com>
2026-02-18 15:25:43 +0530

    lab - 6: Initial commit

--- StudentApi/src/main/java/com/Calculator.java
+++ StudentApi/src/main/java/com/Calculator.java
@@ -2,11 +2,15 @@ package com;
 
 public class Calculator {
 
-    public int add(int a, int b) {
+	public int add(int a, int b) {
         return a + b;
-    }
+	}
 
-    public int subtract(int a, int b) {
-        return a - b;
-    }
+	public boolean isPositive(int number) {
+		return number > 0;
+	}
+
+	public String getMessage() {
+		return "Hello Student";
+	}
 }
--- StudentApi/src/test/java/com/CalculatorTest.java
+++ StudentApi/src/test/java/com/CalculatorTest.java
@@ -6,20 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
 
     @Test
-    void testAddition() {
+	void testEquals() {
+		Calculator calc = new Calculator();
+		assertEquals(4, calc.add(2, 2));
+	}
 
-        Calculator calc = new Calculator();
-        int result = calc.add(2, 2);
+	@Test
+	void testTrue() {
+		Calculator calc = new Calculator();
+		assertTrue(calc.isPositive(5));
+	}
 
-        assertEquals(4, result);
-    }
+	@Test
+	void testFalse() {
+		Calculator calc = new Calculator();
+		assertFalse(calc.isPositive(-3));
+	}
 
-    @Test
-    void testSubtraction() {
-
-        Calculator calc = new Calculator();
-        int result = calc.subtract(5, 3);
-
-        assertEquals(2, result);
-    }
+	@Test
+	void testNotNull() {
+		Calculator calc = new Calculator();
+		assertNotNull(calc.getMessage());
+	}
 }
