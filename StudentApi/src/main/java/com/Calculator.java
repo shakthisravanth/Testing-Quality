132abcb71d94ec75eb4ce1099ba2e0fc933d41ef
J SHAKTHI SRAVANTH <shakthisravanth001@gmail.com>
2026-02-18 12:42:06 +0530

    lab - 5: Initial commit

--- StudentApi/src/main/java/com/Calculator.java
+++ StudentApi/src/main/java/com/Calculator.java
@@ -2,8 +2,11 @@ package com;
 
 public class Calculator {
 
-	public int add(int a, int b) {
-		return a + b;
-	}
-    
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
--- StudentApi/src/test/java/com/CalculatorTest.java
+++ StudentApi/src/test/java/com/CalculatorTest.java
@@ -5,13 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-	@Test
-	void testAddition() {
+    @Test
+    void testAddition() {
 
-	Calculator calc = new Calculator();
+        Calculator calc = new Calculator();
+        int result = calc.add(2, 2);
 
-	int result = calc.add(2, 2);
+        assertEquals(4, result);
+    }
 
-		assertEquals(4, result);
-	}
+    @Test
+    void testSubtraction() {
+
+        Calculator calc = new Calculator();
+        int result = calc.subtract(5, 3);
+
+        assertEquals(2, result);
+    }
 }
